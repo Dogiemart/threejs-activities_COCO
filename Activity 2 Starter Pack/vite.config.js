1af37c9afdfe3cb8a7ff0d3abcb406c7248264f8
Dogiemart <149856095+Dogiemart@users.noreply.github.com>
2025-10-23 00:37:37 +0800

    sd

--- Activity 2 Starter Pack/vite.config.js
+++ Activity 2 Starter Pack/vite.config.js
@@ -1,18 +1,6 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
-
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-}
+  root: '.', // make sure Vite uses the current folder
+  build: {
+    outDir: 'dist', // output folder for Vercel
+  },
+};
